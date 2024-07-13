5750e8babe20ee25a38d5960f561ac8b35815415
Zenith-One <the.number.zero@gmail.com>
2024-07-13 13:16:39 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+const defaultTime = 10
 
 function App() {
+  const [isRunning,setRunning] = useState(false);
+  const [time, setTime] = useState(defaultTime);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isRunning && time > 0) {
+        setTime((prevTime) => prevTime - 1);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [isRunning,time]);
+
+  const minutes = Math.floor(time / 60);
+  const minutesTens = Math.floor(minutes / 10);
+  const minutesOnes = minutes % 10;
+  const seconds = time % 60;
+  const secondsTens = Math.floor(seconds / 10);
+  const secondsOnes = seconds % 10;
+
+
+  let display = () => {
+    if (isRunning && time > 0) {
+      return (
+        <h1
+          onClick={() => setRunning(false)}
+        >
+          <span>{minutesTens}</span>
+          <span>{minutesOnes}</span>
+          <span>:</span>
+          <span>{secondsTens}</span>
+          <span>{secondsOnes}</span>
+        </h1>
+      );
+    } else if (time === 0) {
+      return (
+        <h1
+          onClick={() => {
+            setRunning(true);
+            setTime(defaultTime);
+          }}
+          style = {{
+            cursor: 'pointer',
+            fontSize: '0.9em'
+          }}>
+            Time's up!
+        </h1>
+      );
+    } else {
+      return (
+        <h1
+          onClick={() => setRunning(true)}
+          style = {{
+            cursor: 'pointer'
+          }}>
+            { time === defaultTime ? 'Start' : 'Resume' }
+
+        </h1>
+      );
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {display()}
       </header>
     </div>
   );
